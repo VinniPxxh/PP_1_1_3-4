e1f5294faa7f551d872736632c94d17fc440a2a4
VinniPxxh <kuznetsove2001@mail.ru>
2022-05-07 13:34:57 +0300

    getSessionFactory

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,11 +1,14 @@
 package jm.task.core.jdbc.dao;
 
+import com.mysql.cj.xdevapi.SessionFactory;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
 
+    private final SessionFactory sessionFactory = Util.getSessionFactory();
     public UserDaoHibernateImpl() {
 
     }
